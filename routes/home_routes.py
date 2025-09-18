481be90ab05c980c7e4ae19c19e3f536eee5492e
Haris <sharisroy@gmail.com>
2025-09-19 02:14:30 +0600

    add a page

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -1,19 +1,9 @@
-from flask import Blueprint
-from utils.response_utils import success_response
+from flask import Blueprint, render_template
 
-# only url_prefix, no strict_slashes here
 home_bp = Blueprint("home", __name__, url_prefix="")
 
+# Serve the static HTML page on base URL
 @home_bp.route("/", methods=["GET"], strict_slashes=False)
 @home_bp.route("/home", methods=["GET"], strict_slashes=False)
 def home():
-    data = {
-        "description": "This is your first API response.",
-        "note": "This project is designed to help you practice both manual and automated API testing.",
-        "next_steps": [
-            "Signup → Register with name, phone, email, and password to create a new account.",
-            "Login → Authenticate using email and password to receive an access token.",
-            "Profile → Retrieve your profile details using the provided access token."
-        ]
-    }
-    return success_response("The Home API endpoint is working successfully.", data)
+    return render_template("index.html")
